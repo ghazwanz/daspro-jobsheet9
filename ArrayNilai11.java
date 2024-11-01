b3e275ffa84727ce9b2c8fa02de346984ab8e574
Ghazwan Ababil <ababilghazwan@gmail.com>
2024-11-01 08:51:03 +0700

    Percobaan 2 Pertanyaan 4

--- ArrayNilai11.java
+++ ArrayNilai11.java
@@ -13,7 +13,7 @@ public class ArrayNilai11 {
         for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Mahasiswa ke-" + i + " lulus!");
-            }
+            } else System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
         }
     }
 }
